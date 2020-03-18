d3bd4415bb17d56a9a3505807ddbb8987a5c0085
Mani <m.tahriri.z@gmail.com>
2020-03-19 00:49:38 +0100

    ready for express

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-// main core modules of node js
 
-// 1- http & https -> creating server and work with http request and responses
-// fs -> file system
-// path -> constructing path to file whch work in all operating system
-// os -> working with operating system
-
-// now we want to use http in core nodejs
 const http = require('http');
 
-const routs = require('./routs');
+const express = require('express');
 
-const server = http.createServer(routs);
+const expressapp = express();
+
+const server = http.createServer();
 
 server.listen(3000);
