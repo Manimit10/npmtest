f08e0de095d3d2aeb514f0d77fbca40e4998323c
Mani <m.tahriri.z@gmail.com>
2020-03-23 00:53:04 +0100

    views and util added

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 
 const express = require('express');
+
+const path = require('path');
+
 const bodyparser = require('body-parser');
 
 const app = express();
@@ -13,7 +16,7 @@ app.use('/admin' ,adminRoutes);
 app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
-    res.status(404).send('<h1>Page not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 // const server = http.createServer(app);
