48e3efb5bffc18048d599787e267140f5c7a8bd4
Mani <m.tahriri.z@gmail.com>
2020-03-22 17:49:17 +0100

    routs created

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 
-const http = require('http');
-
 const express = require('express');
+const bodyparser = require('body-parser');
 
-const expressapp = express();
+const app = express();
 
-const server = http.createServer();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000);
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.use('/admin' ,adminRoutes);
+app.use(shopRoutes);
+
+app.use('/', (req, res, next) => {
+    res.status(404).send('<h1>Page not Found</h1>')
+});
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+
+app.listen(3000);
---
+++ routs/admin.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/admin/add-product',(req, res, next) => {
+    res.send('<form action="/add-product" method="POST"><input type= "text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+router.post('/admin/add-product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+
+});
+
+module.exports = router;
