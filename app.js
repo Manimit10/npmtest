1da212c6f442ce6a9f26f1256fe03d8604a70812
Mani <m.tahriri.z@gmail.com>
2020-03-23 16:50:12 +0100

    Pug file added

--- app.js
+++ app.js
@@ -7,16 +7,20 @@ const bodyparser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyparser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin' ,adminRoutes);
 app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404');
 });
 
 // const server = http.createServer(app);
